3eeac08cadb7a85c9621a9e9a989221a987736c7
LATL9 <113098926+LATL9@users.noreply.github.com>
2024-01-26 11:44:07 +0000

    auto saved

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
-//#include <array>
-//#include <cmath>
+#include <array>
+#include <cmath>
 #include <fstream>
-//#include <iostream>
-//#include <time.h>
-//#include <vector>
-//
+#include <iostream>
+#include <time.h>
+#include <vector>
+
 #include "torch/torch.h"
 
 #include "common.hpp"
 #include "controls.hpp"
 #include "structs.hpp"
 
-//void load_model(torch::device device, torch::nn::Sequential& model)
-//{
-//    auto checkpoint = torch.load("models/model.pt", map_location=device)
-//    model.load_state_dict(checkpoint['model_state_dict'])
-//    model.to(device)
-//}
+void load_model(torch::device device, torch::nn::Sequential& model)
+{
+    auto checkpoint = torch.load("models/model.pt", map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+}
 
 void get_bullets(std::vector<bullet>& bullets)
 {
@@ -34,125 +34,125 @@ void get_bullets(std::vector<bullet>& bullets)
     bullets.erase(bullets.begin());
 }
 
-//void get_input(torch::Tensor& input, player p, std::vector<bullet> bullets)
-//{
-//    float b_x;
-//    float b_y;
-//    float p_x = p.pos.x + PLAYER_SIZE / 2
-//    float p_y = p.pos.y + PLAYER_SIZE / 2
-//    int x;
-//    int y;
-//
-//    for (size_t i = 0; i < self.bullets.size(); ++i)
-//    {
-//        b_x = bullets[i].pos.x + bullets[i].size.x / 2
-//        b_y = bullets[i].pos.y + bullets[i].size.y / 2
-//        if (b_x - p_x >= WIDTH / -3 &&
-//            b_x - p_x < WIDTH / 3 &&
-//            b_y - p_y >= HEIGHT / -3 &&
-//            b_y - p_y < HEIGHT / 3)
-//        {
-//            x = (int)((((b_x - p_x) / (WIDTH / 3)) + 1) * (INPUT_SIZE / 2))
-//            y = (int)((((b_y - p_y) / (HEIGHT / 3)) + 1) * (INPUT_SIZE / 2))
-//            for (size_t y_2 = -2; y_2 < 3; ++y)
-//            {
-//                for (size_t x_2 = -2; y_2 < 3; ++y)
-//                {
-//                    if (pow(pow(x_2, 2) + pow(y_2, 2), 0.5) <= 2.5)
-//                    {
-//                        input[std::max(std::min(y + y_2, INPUT_SIZE - 1), 0)][std::max(std::min(x + x_2, INPUT_SIZE - 1), 0)] = 1
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//void get_action(torch::nn::Sequential model, torch::Tensor input, std::array& output)
-//{
-//    torch::Tensor y = model(input);
-//    int32_t* y_ptr = input.data_ptr<int32_t>();
-//    std::vector<int32_t> y_vector{r_ptr, r_ptr + input.?};
-//
-//    for (size_t i = 0; i < FRAMES_PER_ACTIONl ++i)
-//    {
-//        for (size_t j = 0; j < 4; ++j)
-//        {
-//            if (y_vector[i * 4 + j] > ACTION_THRESHOLD) { output[i][j] = 1; }
-//        }
-//        for (size_t j = 0; j < 4; j += 2)
-//        {
-//            if (output[i][j] == 1 and output[i][j + 1] == 1)
-//            {
-//                if (y_vector[i * 4 + j] > j_vector[i * 4 + j + 1])
-//                {
-//                    output[i][j + 1] = 0
-//                } else {
-//                    output[i][j] = 0
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//void exec_action(std::array output, clock_t time, std::array KEYS)
-//{
-//    for (size_t i = 0; i < FRAMES_PER_ACTION; ++i)
-//    {
-//            while ((double)(clock() - time) / CLOCKS_PER_SEC < FRAME_TIME + ACTION_TIME * (i + 1)) { continue; }
-//            for (size_t j = 0; j < 4; ++j)
-//            {
-//                XTestFakeKeyEvent(display, keys[j], output[i][j], 0);
-//            }
-//    }
-//}
+void get_input(torch::Tensor& input, player p, std::vector<bullet> bullets)
+{
+    float b_x;
+    float b_y;
+    float p_x = p.pos.x + PLAYER_SIZE / 2
+    float p_y = p.pos.y + PLAYER_SIZE / 2
+    int x;
+    int y;
+
+    for (size_t i = 0; i < self.bullets.size(); ++i)
+    {
+        b_x = bullets[i].pos.x + bullets[i].size.x / 2
+        b_y = bullets[i].pos.y + bullets[i].size.y / 2
+        if (b_x - p_x >= WIDTH / -3 &&
+            b_x - p_x < WIDTH / 3 &&
+            b_y - p_y >= HEIGHT / -3 &&
+            b_y - p_y < HEIGHT / 3)
+        {
+            x = (int)((((b_x - p_x) / (WIDTH / 3)) + 1) * (INPUT_SIZE / 2))
+            y = (int)((((b_y - p_y) / (HEIGHT / 3)) + 1) * (INPUT_SIZE / 2))
+            for (size_t y_2 = -2; y_2 < 3; ++y)
+            {
+                for (size_t x_2 = -2; y_2 < 3; ++y)
+                {
+                    if (pow(pow(x_2, 2) + pow(y_2, 2), 0.5) <= 2.5)
+                    {
+                        input[std::max(std::min(y + y_2, INPUT_SIZE - 1), 0)][std::max(std::min(x + x_2, INPUT_SIZE - 1), 0)] = 1
+                }
+            }
+        }
+    }
+}
+
+void get_action(torch::nn::Sequential model, torch::Tensor input, std::array& output)
+{
+    torch::Tensor y = model(input);
+    int32_t* y_ptr = input.data_ptr<int32_t>();
+    std::vector<int32_t> y_vector{r_ptr, r_ptr + input.?};
+
+    for (size_t i = 0; i < FRAMES_PER_ACTIONl ++i)
+    {
+        for (size_t j = 0; j < 4; ++j)
+        {
+            if (y_vector[i * 4 + j] > ACTION_THRESHOLD) { output[i][j] = 1; }
+        }
+        for (size_t j = 0; j < 4; j += 2)
+        {
+            if (output[i][j] == 1 and output[i][j + 1] == 1)
+            {
+                if (y_vector[i * 4 + j] > j_vector[i * 4 + j + 1])
+                {
+                    output[i][j + 1] = 0
+                } else {
+                    output[i][j] = 0
+                }
+            }
+        }
+    }
+}
+
+void exec_action(std::array output, clock_t time, std::array KEYS)
+{
+    for (size_t i = 0; i < FRAMES_PER_ACTION; ++i)
+    {
+            while ((double)(clock() - time) / CLOCKS_PER_SEC < FRAME_TIME + ACTION_TIME * (i + 1)) { continue; }
+            for (size_t j = 0; j < 4; ++j)
+            {
+                XTestFakeKeyEvent(display, keys[j], output[i][j], 0);
+            }
+    }
+}
 
 int main()
 {
     controls ctrls;
     const std::array<unsigned int, 4> KEYS = ctrls.get_keys();
 
-//    torch::nn::Sequential model = torch::nn::Sequential(
-//        torch::nn::ConstantPad2d(7, 1),
-//        torch::nn::Conv2d(2, 16, kernel_size=(15, 15)),
-//        torch::nn::LeakyReLU(),
-//        torch::nn::MaxPool2d((2, 2), stride=2),
-//        torch::nn::ConstantPad2d(3, 1),
-//        torch::nn::Conv2d(16, 64, kernel_size=(7, 7)),
-//        torch::nn::LeakyReLU(),
-//        torch::nn::MaxPool2d((2, 2), stride=2),
-//        torch::nn::ConstantPad2d(1, 1),
-//        torch::nn::Conv2d(64, 128, kernel_size=(3, 3)),
-//        torch::nn::LeakyReLU(),
-//        torch::nn::MaxPool2d((2, 2), stride=2),
-//        torch::nn::Flatten(1, 3),
-//        torch::nn::Linear(2048, 1024),
-//        torch::nn::LeakyReLU(),
-//        torch::nn::Linear(1024, 256),
-//        torch::nn::LeakyReLU(),
-//        torch::nn::Linear(256, 64),
-//        torch::nn::LeakyReLU(),
-//        torch::nn::Linear(64, 4 * FRAMES_PER_ACTION),
-//        torch::nn::Sigmoid(),
-//        torch::nn::ReLU()
-//    )
+    torch::nn::Sequential model = torch::nn::Sequential(
+        torch::nn::ConstantPad2d(7, 1),
+        torch::nn::Conv2d(2, 16, kernel_size=(15, 15)),
+        torch::nn::LeakyReLU(),
+        torch::nn::MaxPool2d((2, 2), stride=2),
+        torch::nn::ConstantPad2d(3, 1),
+        torch::nn::Conv2d(16, 64, kernel_size=(7, 7)),
+        torch::nn::LeakyReLU(),
+        torch::nn::MaxPool2d((2, 2), stride=2),
+        torch::nn::ConstantPad2d(1, 1),
+        torch::nn::Conv2d(64, 128, kernel_size=(3, 3)),
+        torch::nn::LeakyReLU(),
+        torch::nn::MaxPool2d((2, 2), stride=2),
+        torch::nn::Flatten(1, 3),
+        torch::nn::Linear(2048, 1024),
+        torch::nn::LeakyReLU(),
+        torch::nn::Linear(1024, 256),
+        torch::nn::LeakyReLU(),
+        torch::nn::Linear(256, 64),
+        torch::nn::LeakyReLU(),
+        torch::nn::Linear(64, 4 * FRAMES_PER_ACTION),
+        torch::nn::Sigmoid(),
+        torch::nn::ReLU()
+    )
 
     std::vector<bullet> bullets;
     torch::Tensor input = torch::full({ 2, 32, 32 }, 0);
     std::array<std::array<unsigned int, 4>, FRAMES_PER_ACTION> output;
     clock_t time;
 
-    //load_model(torch::device("cpu"), model);
+    load_model(torch::device("cpu"), model);
 
     while (true)
     {
         time = clock();
         get_bullets(bullets);
-        //get_input(input[0]);
+        get_input(input[0]);
         while ((double)(clock() - time) / CLOCKS_PER_SEC < FRAME_TIME) { continue; }
         get_bullets(bullets);
-        //get_input(input[1]);
+        get_input(input[1]);
 
-        //get_action(model, input, output);
+        get_action(model, input, output);
         ctrls.exec_action(output, time, KEYS);
         while ((double)(clock() - time) / CLOCKS_PER_SEC < FRAME_TIME + ACTION_TIME) { continue; }
     }
