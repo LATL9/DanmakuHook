39a08fda5a6616db3d7b62cbb8c8c311f101dc6a
LATL9 <113098926+LATL9@users.noreply.github.com>
2024-01-27 22:42:21 +0000

    auto saved

--- src/controls.cpp
+++ src/controls.cpp
@@ -37,7 +37,7 @@ void controls::exec_action(std::array<std::array<unsigned int, 4>, FRAMES_PER_AC
     {
         if (output[0][i])
         {
-            std::cout << "";
+            std::cout << " ";
             switch (i)
             {
                 case 0:
