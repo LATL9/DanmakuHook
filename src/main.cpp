801695b998ee8d35f25a59eafc574d71346cd4f9
LATL9 <113098926+LATL9@users.noreply.github.com>
2024-01-27 23:15:51 +0000

    auto saved

--- src/main.cpp
+++ src/main.cpp
@@ -120,20 +120,28 @@ void get_action(torch::jit::script::Module model, torch::Tensor input, std::arra
             switch (output[i][j])
             {
                 case 0:
-                    std::cout << "↑";
+                    std::cout << " ";
                     break;
 
-                case 1:
-                    std::cout << "↓";
-                    break;
+                default:
+                    switch (j)
+                    {
+                        case 0:
+                            std::cout << "↑";
+                            break;
 
-                case 2:
-                    std::cout << "←";
-                    break;
+                        case 1:
+                            std::cout << "↓";
+                            break;
 
-                case 3:
-                    std::cout << "→";
-                    break;
+                        case 2:
+                            std::cout << "←";
+                            break;
+
+                        case 3:
+                            std::cout << "→";
+                            break;
+                    }
             }
             std::cout << '\n';
         }
